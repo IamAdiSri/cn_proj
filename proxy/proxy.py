3f5396a60dd375b0cf04f482f19f312abaf6310b
Aditya Srivastava <aditya.srivastava@research.iiit.ac.in>
2017-04-14 00:31:18 +0530

    Added cache functionality

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -12,6 +12,7 @@ import sys
 import threading
 import time
 # import requests
+# import httplib
 
 cache = {}
 
@@ -68,6 +69,130 @@ def bl_check(host):
     
     return flag
 
+def cache_check(url, conn, client_req):
+    print "??? Checking if requested page has been cached"
+    TIMEOUT = 5 * 60
+    global cache
+    t = "time"
+    c = "calls"
+
+    orig_url = url
+    url_file = ""
+    for i in range(len(url)):
+        if url[i] != "/":
+            url_file += url[i]
+
+    if url not in cache or time.time() - cache[orig_url][t] >= TIMEOUT:
+        entry = {t: time.time(), c: 1}
+        cache[orig_url] = entry
+        return False
+    
+    cache[orig_url][c] += 1
+        
+    if cache[orig_url][c] < 4:
+        return False
+
+    req = client_req.split("\r\n")
+
+    host = req[1].split(":")[1][1:]
+    if len(req[1].split(":")) < 3:
+        port = 80
+    else:
+        port = int(req[1].split(":")[2])
+
+    print "??? Opening socket to end server at", host+":"+str(port)
+    sock = socket.socket()
+    sock.connect((host, port))
+
+    print "??? Forwarding request on behalf of client to origin server at", url
+    
+    if host == "localhost" or host == "127.0.0.1":
+        print "??? Origin server is located locally"
+
+        method = req[0].split(" ")[0]
+        
+        http_pos = url.find("://")
+        if http_pos != -1:
+            url = url[(http_pos + 3):]
+        
+        file_pos = url.find("/")
+        url = url[file_pos:]
+
+        http_ver = req[0].split(" ")[2]
+
+        req[0] = "%s %s %s" % (method, url, http_ver)
+
+        if cache[orig_url][c] > 4:
+            req.insert(2, "If-Modified-Since: %s" % (time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime(cache[orig_url][c]))))
+
+        new_req = ""
+        for l in req:
+            new_req += (l + "\r\n")
+
+        print new_req
+        sock.send(new_req)
+
+    else:
+        print "??? Origin server is located externally"
+
+        req = client_req.split("\r\n")
+        if cache[orig_url][c] > 4:
+            req.insert(2, "If-Modified-Since: %s" % (time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime(cache[orig_url][c]))))
+
+        client_req = ""
+        for l in req:
+            client_req += (l + "\r\n")
+
+        print client_req
+        sock.send(client_req)
+        
+    print "??? Recieving response from origin server"
+    response = sock.recv(1024)
+    change = False
+    if "304" in response.split("\r\n"):
+        change = True
+    print response
+
+    print "??? Forwarding response to client"
+    temp = response.split("\r\n")
+    if not change:
+        conn.send(response)
+    else:
+        temp2 = temp[0].split(" ")
+        temp2[1] = "200"
+        temp2[2] = "OK"
+        temp2 = " ".join(temp2)
+        temp[0] = temp2
+        response = "\r\n".join(temp)
+        conn.send(response)
+
+    if cache[orig_url][c] > 4:
+        print "??? Checking if file has been modified at origin server and sending cached file if not modified"
+        if change == True:
+            cache[orig_url][c] = 4
+        else:
+            print "??? File has not been modified; sending from cache"
+            with open(url_file, 'r') as f:
+                while True:
+                    data = f.read(1024)
+                    conn.send(data)
+                    if not data:
+                        break
+    if cache[orig_url][c] == 4:
+        print "??? Recieving fresh data from origin server and forwarding to client"
+        cache[orig_url][t] == time.time()
+        with open(url_file, 'wb') as f:
+            while True:
+                data = sock.recv(1024)
+                f.write(data)
+                conn.send(data)
+                if not data:
+                    break
+
+    print "??? Client request fulfilled"
+    return True
+
+    
 def request_handler(conn, addr):
     client_req = conn.recv(1024)
     # print client_req
@@ -83,13 +208,21 @@ def request_handler(conn, addr):
 
     if bl_check(host): # checking if domain is blacklisted
         conn.send("HTTP/1.1 403.6 Forbidden\r\nServer: proxy_server Python/2.7\r\n\r\n")
-        conn.send("""<html>403 Forbidden\nIP has been blacklisted by proxy server\n</html>""")
+        conn.send("<html>403 Forbidden\nIP has been blacklisted by proxy server\n</html>")
         print "??? Domain referred to is blacklisted and will not be accessed"
         print "??? Closing connection to client"
         conn.close()
         print "??? Exiting thread"
         print "??? --------------------------------------------------\n\n"
         exit()
+
+    if cache_check(url, conn, client_req):
+        print "??? Closing connection to client"
+        conn.close()
+        print "??? Exiting thread"
+        print "??? --------------------------------------------------\n\n"
+        exit()
+    print "??? Page hasn't been cached yet"
 
     # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     # r = requests.post(url)
